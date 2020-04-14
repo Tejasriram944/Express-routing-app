726fd749a9a0121c880ece741801e4cd880089a2
TEJA SRIRAM <49815246+Tejasriram944@users.noreply.github.com>
2020-04-14 12:18:55 +0530

    files running on server

--- App.js
+++ App.js
@@ -2,15 +2,18 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-let hostName = '127.0.0.1';
-let port = 3000;
+const hostName = '127.0.0.3';
+const port = 3000;
 
 // get static files load
-app.use('/static', express.static('public'));
+app.use('/public', express.static('public'));
 
 // get html file
-app.get('/',(req,res) =>{
-    res.sendFile(path.join(__dirname,'index.html'));
+app.get('/',(req,res) => {
+    res.sendFile(path.join(__dirname,'views','index.html'));
+});
+app.get('/404',(req,res) =>{
+    res.sendFile(path.join(__dirname,'views','notfound.html'));
 });
 
 app.listen(port,hostName,() =>{
